edc9c7d8e7645fc09eb24fa0894308480c0e0451
Kimiblock Moe <kimiblock@icloud.com>
2026-08-31 15:39:48 +0800

    initialise module file_list

---
+++ src/file_list.rs
@@ -0,0 +1,41 @@
+/**
+	The public trait GetFileList is implemented by multiple backends
+*/
+pub trait GetFileList {
+	async fn list(&self) -> Vec<PackageFile>;
+}
+
+/**
+	The public enum PackageFile represents a file in a package.
+
+	It describes several key information to implement the "copy" action.
+*/
+pub enum PackageFile {
+	Regular {
+		/**
+			source_path describes the current file path on disk.
+		*/
+		source_path:	std::path::PathBuf,
+
+		/**
+			dest_path describes the destination to which the file would be installed.
+
+			The parent path will be automatically created.
+		*/
+		dest_path:	std::path::PathBuf,
+	},
+
+	Symlink {
+		/**
+			dest_path describes the destination to which the link would be installed.
+
+			The parent path will be automatically created.
+		*/
+		dest_path:	std::path::PathBuf,
+
+		/**
+			link_target should be a path describing the link target
+		*/
+		link_target:	std::path::PathBuf,
+	}
+}
