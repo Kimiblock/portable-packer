a44ace258bea3ce5dfc0c138a84003210dc8d53a
Kimiblock Moe <kimiblock@icloud.com>
2026-09-02 11:48:26 +0800

    define the trait PostInstall and it's method binary

--- src/distro/post_install/traits.rs
+++ src/distro/post_install/traits.rs
@@ -1,1 +1,10 @@
-pub trait PostInstall {}
+pub trait PostInstall {
+	/**
+		Overlay the contents in bin into app-private copy of OverlayFS.
+
+		Deletes original binaries, and generates a new stub one for launching the sandbox.
+	*/
+	async fn binary(&self, overlay: bool) -> Result<(), Self::PostError>;
+
+	type PostError;
+}
