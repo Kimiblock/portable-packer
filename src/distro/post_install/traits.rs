96d5180783206cc92a8a72cef24539b2e36296e6
Kimiblock Moe <kimiblock@icloud.com>
2026-09-02 11:57:55 +0800

    desktop_file: define trait

--- src/distro/post_install/traits.rs
+++ src/distro/post_install/traits.rs
@@ -3,8 +3,22 @@ pub trait PostInstall {
 		Overlay the contents in bin into app-private copy of OverlayFS.
 
 		Deletes original binaries, and generates a new stub one for launching the sandbox.
+
+		The resulting String would be that binary name.
 	*/
-	async fn binary(&self, overlay: bool) -> Result<(), Self::PostError>;
+	async fn binary(&self, overlay: bool) -> Result<String, Self::PostError>;
+
+	/**
+		Removes any .desktop file that is installed in the package root.
+
+		Installs the new one into package root.
+	*/
+	async fn desktop_file(
+		&self,
+		app_id:		&str,
+		desktop_file:	std::path::PathBuf,
+	) -> Result<(), Self::PostError>;
 
 	type PostError;
 }
+
