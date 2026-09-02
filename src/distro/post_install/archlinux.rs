96157a52f2bf418ccc3ebeb1c52ae5f00f4619d2
Kimiblock Moe <kimiblock@icloud.com>
2026-09-02 12:24:24 +0800

    implement the default trait for Arch Post

--- src/distro/post_install/archlinux.rs
+++ src/distro/post_install/archlinux.rs
@@ -1,4 +1,16 @@
 /**
-	This struct implements post install function for Arch Linix
+	This struct implements post install function for Arch Linux
 */
-pub struct ArchPost {}
+pub struct ArchPost {
+	pkgdir:	std::sync::Arc<String>,
+}
+
+impl Default for ArchPost {
+	fn default() -> Self {
+		let pkgdir = std::env::var("pkgdir").expect("Could not get pkgdir from env");
+
+		Self {
+			pkgdir:	std::sync::Arc::new(pkgdir),
+		}
+	}
+}
