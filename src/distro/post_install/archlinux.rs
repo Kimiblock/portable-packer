1abc2fd82169cae9b3dd68344f4ac71577e5aa4b
Kimiblock Moe <kimiblock@icloud.com>
2026-09-03 14:03:23 +0800

    archlinux: implement the desktop_file function in trait

--- src/distro/post_install/archlinux.rs
+++ src/distro/post_install/archlinux.rs
@@ -2,7 +2,7 @@
 	This struct implements post install function for Arch Linux
 */
 pub struct ArchPost {
-	pkgdir:	std::sync::Arc<String>,
+	pkgdir:	std::sync::Arc<std::path::PathBuf>,
 }
 
 impl Default for ArchPost {
@@ -10,7 +10,61 @@ impl Default for ArchPost {
 		let pkgdir = std::env::var("pkgdir").expect("Could not get pkgdir from env");
 
 		Self {
-			pkgdir:	std::sync::Arc::new(pkgdir),
+			pkgdir:	std::sync::Arc::new(
+				std::path::PathBuf::from(pkgdir)
+			),
 		}
 	}
+}
+
+
+#[derive(thiserror::Error, Debug)]
+pub enum ArchError {
+	#[error("I/O error removing .desktop files: {0:#?}")]
+	DesktopFileRmIOError(std::io::Error),
+
+	#[error("I/O error installing .desktop file: {0:#?}")]
+	DesktopFileInstallIOError(std::io::Error),
+}
+
+async fn desktop_file(
+	post:		&ArchPost,
+	app_id:		&str,
+	desktop_file:	std::path::PathBuf,
+) -> Result<(), ArchError> {
+	let desktop_path = {
+		let mut path = post.pkgdir.to_path_buf();
+		path.push("usr");
+		path.push("share");
+		path.push("applications");
+		path
+	};
+
+	if desktop_path.exists() {
+		tokio::fs::remove_dir_all(&desktop_path)
+			.await
+			.map_err(ArchError::DesktopFileRmIOError)
+			?
+	};
+
+	tokio::fs::create_dir_all(&desktop_path)
+		.await
+		.map_err(ArchError::DesktopFileInstallIOError)
+		?;
+
+	tokio::fs::copy(
+		desktop_file,
+		{
+			let mut path = desktop_path.to_path_buf();
+			let mut name = String::from(app_id);
+			name.push_str(".desktop");
+			path.push(&name);
+			path
+		},
+	)
+		.await
+		.map_err(ArchError::DesktopFileInstallIOError)
+		?;
+
+	Ok(())
 }
